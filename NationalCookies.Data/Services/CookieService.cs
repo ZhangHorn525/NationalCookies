ab28ab341c95522c1b0d6ccea3a7b6ca3ed48ce2
Horn Zhang <ZHANHOR@chrobinson.com>
2021-03-31 08:45:42 +0800

    RedisBranch

--- NationalCookies.Data/Services/CookieService.cs
+++ NationalCookies.Data/Services/CookieService.cs
@@ -1,26 +1,46 @@
-﻿using NationalCookies.Data.Interfaces;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using NationalCookies.Data.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace NationalCookies.Data.Services
 {
     public class CookieService : ICookieService
     {
         private CookieContext _context;
+        private IDistributedCache _cache;
 
-        public CookieService(CookieContext context)
+        public CookieService(CookieContext context,IDistributedCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public List<Cookie> GetAllCookies()
         {
             List<Cookie> cookies;
 
-            //get the cookies from the database
-            cookies = _context.Cookies.ToList();
+            var cachedCookies = _cache.GetString("cookies");
+            if (!string.IsNullOrEmpty(cachedCookies))
+            {
+                cookies = JsonConvert.DeserializeObject<List<Cookie>>(cachedCookies);
+            }
+            else {
+                //get the cookies from the database
+                cookies = _context.Cookies.ToList();
+
+                DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+                options.SetAbsoluteExpiration(new System.TimeSpan(0,0,15));
+
+                _cache.SetString("cookies",JsonConvert.SerializeObject(cookies),options);
+            }
 
             return cookies;
+        }
+
+        public void ClearCache() {
+            _cache.Remove("cookies");
         }
     }
 }
